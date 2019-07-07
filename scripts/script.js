dc589caee37289c6b25a56882ed4f05eca244692
ryanrestivo <ryan.restivo@gmail.com>
2019-07-07 14:52:01 -0400

    setup function for resume job titles

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,14 @@ console.log(LINK.getAttribute("target"));
 } else {
 	LINK.setAttribute("target", "_blank");
 }
+
+function Job(title,place,duration) {
+  this.title = title;
+  this.place = place;
+  this.duration = duration;
+}
+
+var jobNewsday2 = new Job("Project Manager, Mobile Apps","Newsday","January 2019 - Present");
+var jobNewsday1 = new Job("Editorial Development Coordinator","Newsday","May 2016 - December 2018");
+var jobMLB = new Job("Content Coordinator","MLB Advanced Media","June 2009 - April 2016");
+var jobESPN = new Job("Quality Assurance Associate","ESPN Digital Media","September 2008 - June 2009");
