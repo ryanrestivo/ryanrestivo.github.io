8fc6de2fc1d60db1f9ec0f474c83bb77e1bcecfc
ryanrestivo <ryan.restivo@gmail.com>
2019-07-07 21:32:00 -0400

    target blank for loop

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,9 @@
-// change first job in array to green - to make it most recent - CSS style change
-document.querySelectorAll(".position")[0].setAttribute("style", "color: green;")
-
-/// currently applies to first a link
-const LINK = document.querySelector(".links a");
-if ((LINK).hasAttribute("target")) {
-console.log(LINK.getAttribute("target"));
-} else {
-	LINK.setAttribute("target", "_blank");
+/// make all links in new window
+var extLinks = document.querySelectorAll('a[href^="http"]');
+for (var i = 0; i < extLinks.length; i++) {
+  if (!extLinks[i].hasAttribute("target")) {
+    extLinks[i].setAttribute("target","_blank");
+  }
 }
 
 function Job(title,place,duration) {
@@ -19,3 +16,6 @@ var jobNewsday2 = new Job("Project Manager, Mobile Apps","Newsday","January 2019
 var jobNewsday1 = new Job("Editorial Development Coordinator","Newsday","May 2016 - December 2018");
 var jobMLB = new Job("Content Coordinator","MLB Advanced Media","June 2009 - April 2016");
 var jobESPN = new Job("Quality Assurance Associate","ESPN Digital Media","September 2008 - June 2009");
+
+// change first job in array to green - to make it most recent - CSS style change
+// document.querySelectorAll(".position")[0].setAttribute("style", "color: green;")
