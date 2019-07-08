bc4227c4f6da274b064c6a174640bdc7f4407c26
ryanrestivo <ryan.restivo@gmail.com>
2019-07-07 22:37:21 -0400

    changing titles of variables in Job function

--- scripts/resume.js
+++ scripts/resume.js
@@ -4,11 +4,11 @@ function Job(title,place,duration) {
   this.duration = duration;
 }
 
-var jobNewsday2 = new Job("Project Manager, Mobile Apps","Newsday","January 2019 - Present");
-var jobNewsday1 = new Job("Editorial Development Coordinator","Newsday","May 2016 - December 2018");
-var jobMLB = new Job("Content Coordinator","MLB Advanced Media","June 2009 - April 2016");
-var jobESPN = new Job("Quality Assurance Associate","ESPN Digital Media","September 2008 - June 2009");
+var job0 = new Job("Project Manager, Mobile Apps","Newsday","January 2019 - Present");
+var job1 = new Job("Editorial Development Coordinator","Newsday","May 2016 - December 2018");
+var job2 = new Job("Content Coordinator","MLB Advanced Media","June 2009 - April 2016");
+var job3 = new Job("Quality Assurance Associate","ESPN Digital Media","September 2008 - June 2009");
 
-document.querySelector("#position" + 1).innerHTML = jobNewsday2.title;
+document.querySelector("#position" + 1).innerHTML = job0.title;
 
-document.querySelector("#position2").innerHTML = jobNewsday1.title;
+document.querySelector("#position2").innerHTML = job1.title;
