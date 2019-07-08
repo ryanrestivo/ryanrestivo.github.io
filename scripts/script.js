1502f546207f3418a1799c39a07b02cb821e5372
ryanrestivo <ryan.restivo@gmail.com>
2019-07-07 22:00:26 -0400

    remove resume from main script

--- scripts/script.js
+++ scripts/script.js
@@ -6,16 +6,5 @@ for (var i = 0; i < extLinks.length; i++) {
   }
 }
 
-function Job(title,place,duration) {
-  this.title = title;
-  this.place = place;
-  this.duration = duration;
-}
-
-var jobNewsday2 = new Job("Project Manager, Mobile Apps","Newsday","January 2019 - Present");
-var jobNewsday1 = new Job("Editorial Development Coordinator","Newsday","May 2016 - December 2018");
-var jobMLB = new Job("Content Coordinator","MLB Advanced Media","June 2009 - April 2016");
-var jobESPN = new Job("Quality Assurance Associate","ESPN Digital Media","September 2008 - June 2009");
-
 // change first job in array to green - to make it most recent - CSS style change
 // document.querySelectorAll(".position")[0].setAttribute("style", "color: green;")
