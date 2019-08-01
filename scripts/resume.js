855f306781e3405a8129c537c57e2cb611a17849
ryanrestivo <ryan.restivo@gmail.com>
2019-07-31 22:06:22 -0400

    make an array of jobs and query select the HTML via loop

--- scripts/resume.js
+++ scripts/resume.js
@@ -4,11 +4,18 @@ function Job(title,place,duration) {
   this.duration = duration;
 }
 
-var job0 = new Job("Project Manager, Mobile Apps","Newsday","January 2019 - Present");
-var job1 = new Job("Editorial Development Coordinator","Newsday","May 2016 - December 2018");
-var job2 = new Job("Content Coordinator","MLB Advanced Media","June 2009 - April 2016");
-var job3 = new Job("Quality Assurance Associate","ESPN Digital Media","September 2008 - June 2009");
+//var job0 = new Job("Project Manager, Mobile Apps","Newsday","January 2019 - Present");
+//var job1 = new Job("Editorial Development Coordinator","Newsday","May 2016 - December 2018");
+//var job2 = new Job("Content Coordinator","MLB Advanced Media","June 2009 - April 2016");
+//var job3 = new Job("Quality Assurance Associate","ESPN Digital Media","September 2008 - June 2009");
 
-document.querySelector("#position" + 1).innerHTML = job0.title;
+// array of jobs
+jobs = [new Job("Project Manager, Mobile Apps","Newsday","January 2019 - Present"), new Job("Editorial Development Coordinator","Newsday","May 2016 - December 2018"), new Job("Content Coordinator","MLB Advanced Media","June 2009 - April 2016"),new Job("Quality Assurance Associate","ESPN Digital Media","September 2008 - June 2009")];
 
-document.querySelector("#position2").innerHTML = job1.title;
+for (let i = 0; i<jobs.length; i++) {
+  document.querySelector("#position" + (i + 1)).innerHTML = jobs[i].title;
+  document.querySelector(".place" + (i + 1)).innerHTML = jobs[i].place;
+}
+
+
+// document.querySelector("#position2").innerHTML = job1.title;
