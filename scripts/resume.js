74a7c741bbac1a410cd21b362146466af6e343a4
ryanrestivo <ryan.restivo@gmail.com>
2019-08-01 10:10:15 -0400

    clean up job script

--- scripts/resume.js
+++ scripts/resume.js
@@ -4,17 +4,13 @@ function Job(title,place,duration) {
   this.duration = duration;
 }
 
-//var job0 = new Job("Project Manager, Mobile Apps","Newsday","January 2019 - Present");
-//var job1 = new Job("Editorial Development Coordinator","Newsday","May 2016 - December 2018");
-//var job2 = new Job("Content Coordinator","MLB Advanced Media","June 2009 - April 2016");
-//var job3 = new Job("Quality Assurance Associate","ESPN Digital Media","September 2008 - June 2009");
-
 // array of jobs
 jobs = [new Job("Project Manager, Mobile Apps","Newsday","January 2019 - Present"), new Job("Editorial Development Coordinator","Newsday","May 2016 - December 2018"), new Job("Content Coordinator","MLB Advanced Media","June 2009 - April 2016"),new Job("Quality Assurance Associate","ESPN Digital Media","September 2008 - June 2009")];
 
 for (let i = 0; i<jobs.length; i++) {
   document.querySelector("#position" + (i + 1)).innerHTML = jobs[i].title;
   document.querySelector(".place" + (i + 1)).innerHTML = jobs[i].place;
+  document.querySelector(".duration" + (i + 1)).innerHTML = jobs[i].duration;
 }
 
 
