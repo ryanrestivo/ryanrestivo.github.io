d9927503845fd36b40efd8970e23822da38d3297
ryanrestivo <ryan.restivo@gmail.com>
2019-08-02 18:43:02 -0400

    add skills array

--- scripts/resume.js
+++ scripts/resume.js
@@ -1,11 +1,12 @@
-function Job(title,place,duration) {
+function Job(title,place,duration,skills) {
   this.title = title;
   this.place = place;
   this.duration = duration;
+  this.skills = skills;
 }
 
 // array of jobs
-jobs = [new Job("Project Manager, Mobile Apps","Newsday","January 2019 - Present"), new Job("Editorial Development Coordinator","Newsday","May 2016 - December 2018"), new Job("Content Coordinator","MLB Advanced Media","June 2009 - April 2016"),new Job("Quality Assurance Associate","ESPN Digital Media","September 2008 - June 2009")];
+jobs = [new Job("Project Manager, Mobile Apps","Newsday","January 2019 - Present",["Set agendas and run daily calls with developers and core stakeholders to advance priorities","Work with senior leadership team to set future priorities for features","Define feature requirements with stakeholders and third party vendors","Project lead of priorities to launch redesigned Newsday iOS/Android app","Work closely with Quality Assurance team to test and prioritize issues in iOS/Android apps","Coordinate between departments to evaluate the project scope of the Newsday iOS/Androidapp redesign"]), new Job("Editorial Development Coordinator","Newsday","May 2016 - December 2018"), new Job("Content Coordinator","MLB Advanced Media","June 2009 - April 2016"),new Job("Quality Assurance Associate","ESPN Digital Media","September 2008 - June 2009")];
 
 for (let i = 0; i<jobs.length; i++) {
   document.querySelector("#position" + (i + 1)).innerHTML = jobs[i].title;
